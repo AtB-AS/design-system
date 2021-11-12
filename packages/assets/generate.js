c08dac5f01add6cca4cd253123f017a053623e71
Tore Stensaker Tefre <tore@tefre.it>
2021-11-12 11:31:00 +0100

    add ncp and format code

--- packages/assets/generate.js
+++ packages/assets/generate.js
@@ -1,64 +1,77 @@
-const fs = require("fs-extra")
 const path = require('path');
+const ncp = require('ncp').ncp;
 
-const { resolve } = require('path');
-const { readdir } = require('fs').promises;
+const {resolve} = require('path');
+const {readdir} = require('fs').promises;
 
 async function getFiles(dir) {
-  const dirents = await readdir(dir, { withFileTypes: true });
-  const files = await Promise.all(dirents.map((dirent) => {
-    const res = resolve(dir, dirent.name);
-    const result = dirent.isDirectory() ? getFiles(res) : res
-    // console.log("result", result)
-    return result;
-  }));
+  const dirents = await readdir(dir, {withFileTypes: true});
+  const files = await Promise.all(
+    dirents.map((dirent) => {
+      const res = resolve(dir, dirent.name);
+      const result = dirent.isDirectory() ? getFiles(res) : res;
+      return result;
+    }),
+  );
 
-  const superResult = Array.prototype.concat(...files)
+  const superResult = Array.prototype.concat(...files);
   // console.log(`Superresult ${superResult}`);
-  
+
   const filesToCopy = [];
 
-    superResult.forEach(file => {
-        filesToCopy.push(file)
-    })
+  superResult.forEach((file) => {
+    filesToCopy.push(file);
+  });
 
-    // console.log(`Files to copy from ${dir}: ${filesToCopy}`)
+  // console.log(`Files to copy from ${dir}: ${filesToCopy}`)
 
-    return filesToCopy;
+  return filesToCopy;
 }
 
 const assets = async (orgId, destinationDirectory) => {
-    console.log(`Copying assets for ${orgId} to ${destinationDirectory}`)
+  console.log(`Copying assets for ${orgId} to ${destinationDirectory}`);
 
-    const vaildOrgIds = ["atb", "nfk"];
+  const vaildOrgIds = ['atb', 'nfk'];
 
-    if (!vaildOrgIds.includes(orgId)) {
-        console.error("Invalid orgId provided, valid orgIds are:", vaildOrgIds);
-        process.exit(1);
-    }
+  if (!vaildOrgIds.includes(orgId)) {
+    console.error('Invalid orgId provided, valid orgIds are:', vaildOrgIds);
+    process.exit(1);
+  }
 
-    const commonFolder = path.join(__dirname, 'src', 'common')
-    const orgFolder = path.join(__dirname, 'src', orgId)
+  const commonFolder = path.join(__dirname, 'src', 'common');
+  const orgFolder = path.join(__dirname, 'src', orgId);
 
-    const commonFiles = await getFiles(commonFolder)
-    const orgFiles = await getFiles(orgFolder)
+  const commonFiles = await getFiles(commonFolder);
+  const orgFiles = await getFiles(orgFolder);
 
-    console.log(`Files to be copied from ${commonFolder}: ${commonFiles}`)
-    console.log(`Files to be copied from ${orgFolder}: ${orgFiles}`)
+  // console.log(`Files to be copied from ${commonFolder}: ${commonFiles}`)
+  // console.log(`Files to be copied from ${orgFolder}: ${orgFiles}`)
 
-    const filesToCopy = commonFiles.concat(orgFiles)
+  const allFilesToBeCopied = commonFiles.concat(orgFiles);
 
-    console.log(filesToCopy)
+  console.log(allFilesToBeCopied);
 
-    filesToCopy.forEach(path => {
-        fs.copy(path, destinationDirectory, err => {
-            if (err) console.log(err)
-            else console.log(`${path} successfully copied to ${destinationDirectory}`)
-        })
-    })
+  allFilesToBeCopied.forEach(async (path) => {
+    const splitPath =
+      path.split(`/src/${orgId}`)[1] ?? path.split(`/src/common`)[1];
+    const destinationPath = destinationDirectory + splitPath;
+    console.log('Aiming for destination', destinationPath);
 
-    console.log("generate-assets complete!")
-    process.exit(0);
-}
+    ncp.limit = 16;
+
+    console.log('limit set');
+
+    ncp(path, destinationPath, function (err) {
+      if (err) {
+        console.log(err);
+      } else console.log(`${path} copied to ${destinationPath}`);
+    });
+
+    console.log('why was the previous lines ignored?');
+  });
+
+  console.log('generate-assets complete!');
+  process.exit(0);
+};
 
 module.exports = assets;
