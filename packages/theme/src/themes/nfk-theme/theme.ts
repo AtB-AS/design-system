c632862f545cf49afc561c94b639f4c912d10487
mortennordseth <morten@nordseth.net>
2022-10-25 13:08:22 +0200

    add backgorund_accent_1 for nfk

--- packages/theme/src/themes/nfk-theme/theme.ts
+++ packages/theme/src/themes/nfk-theme/theme.ts
@@ -68,7 +68,7 @@ const themes: Themes = {
         background_2: contrastColor('#EAEAEA', 'dark'),
         background_3: contrastColor('#E4E4E4', 'dark'),
         background_accent_0: contrastColor('#0181A2', 'light'),
-        background_accent_1: contrastColor('#FFFFFF', 'dark'),
+        background_accent_1: contrastColor('#046073', 'light'),
         background_accent_2: contrastColor('#C5DFE6', 'dark'),
         background_accent_3: contrastColor('#0181A2', 'light'),
         background_accent_4: contrastColor('#FFFFFF', 'dark'),
