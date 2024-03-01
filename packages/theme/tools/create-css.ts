b036261b1c353cc6ee89889fdc551f8716777884
mortennordseth <morten@nordseth.net>
2024-03-01 13:01:30 +0100

    feat: theme and assets for troms

--- packages/theme/tools/create-css.ts
+++ packages/theme/tools/create-css.ts
@@ -1,10 +1,11 @@
 import outputThemes from './create-theme';
 import outputTypography from './create-typo';
-import {AtBThemes, NfkThemes, FRAMThemes} from '../src/themes';
+import {AtBThemes, NfkThemes, FRAMThemes, TFKThemes} from '../src/themes';
 
 Promise.all([
   outputThemes('atb-theme', AtBThemes),
   outputThemes('nfk-theme', NfkThemes),
   outputThemes('fram-theme', FRAMThemes),
+  outputThemes('tfk-theme', TFKThemes),
   outputTypography(),
 ]).then(() => console.log('Written CSS files'), console.error);
