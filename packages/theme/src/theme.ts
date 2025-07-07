160ac7fae26a53ff2036349a293a97a3b7548432
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2025-07-07 13:07:23 +0200

    Make lineStyle required

--- packages/theme/src/theme.ts
+++ packages/theme/src/theme.ts
@@ -65,7 +65,7 @@ export type GeofencingZoneStyle<T = ContrastColor> = {
   color: T;
   fillOpacity: number;
   strokeOpacity: number;
-  lineStyle?: 'solid' | 'dashed';
+  lineStyle: 'solid' | 'dashed';
   layerIndexWeight: number;
 };
 export type GeofencingZoneStyles<T = ContrastColor> = {
